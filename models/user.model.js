b7e47a1c09258b1e3f846260b8b4f9fce1585591
Shahriyor <baxtiyarovich0102@gmail.com>
2025-03-28 07:24:11 +0500

    model yaratildi

---
+++ models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+
+let UserSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        username: {type: String, required: true, unique: true},
+        email: {type: String, required: true, unique: true, validate: [validator.isEmail, "Noto'g'ti email"]},
+        password: {type: String, required: [true, "Parolni kiritish majburiy"]},
+        age: {type: Number}
+    }
+)
+
+
+UserSchema.pre("save", async function (next) {
+    let password = this.password;
+    this.password = await bcrypt.hash(password, 12);
+    console.log(this.password);
+  
+    next();
+});
+
+
+let User = mongoose.model('users', UserSchema)
+
+module.exports = User
